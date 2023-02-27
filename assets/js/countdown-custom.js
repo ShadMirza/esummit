e71ebd5f501b2aa9733a8d8785fa92d6e8bf25f4
Shad <shadmirza100@gmail.com>
2023-02-27 19:58:36 +0530

    Small fixes in layout

---
+++ assets/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 4, 3, 10)}); // year, month, date, hour
+        });
+});		
+
